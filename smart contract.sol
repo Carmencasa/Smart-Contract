0071d5be213adb3c0d2c28738f816b51526f3b3d
Carmencasa <carmencasamassima426@gmail.com>
2025-06-26 23:06:17 +0200

    Add files via upload

---
+++ smart contract.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.20;
+
+contract transaction() {
+    mapping(address => uint) public balances;
+       
+       construction() {
+        balances[msg.sender] = 1000;
+    }
+
+    function transfer(address to, uint amount) public {
+        require(amount > 0; "Sufficient import");
+        require(balances[msg.sender] >= amount, "Insufficient funds");
+        require(to != address(0), "Unknown address");
+
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+
+    function getBalances(address user) public view returns (uint) {
+        return balances[user];
+    }
+}
